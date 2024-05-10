8f4715d2ae1c5af1e207cfaa31e9b16287da29d3
i0Ek3 <kno30826@gmail.com>
2024-05-10 20:37:16 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="pyco",
+    version="beta",
+    description="Code obfuscate and deobfuscate program write by Python.",
+    author="i0Ek3",
+    author_email="kno30826@gmail.com",
+    packages=["pyco"],
+    install_requires=[
+    ],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
