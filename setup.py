3ccf530e8e1efbe4e6d0b39ac09316d005525342
i0Ek3 <kno30826@gmail.com>
2024-05-10 20:40:25 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,8 @@ setup(
     description="Code obfuscate and deobfuscate program write by Python.",
     author="i0Ek3",
     author_email="kno30826@gmail.com",
-    packages=["pyco"],
-    install_requires=[
-    ],
+    packages=[],
+    install_requires=[],
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
