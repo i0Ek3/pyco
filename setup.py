743a05baade1d626c0789a8fd566bed3d2a5981f
i0Ek3 <kno30826@gmail.com>
2024-05-10 20:38:45 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pyco",
-    version="beta",
+    version="0.0.1",
     description="Code obfuscate and deobfuscate program write by Python.",
     author="i0Ek3",
     author_email="kno30826@gmail.com",
